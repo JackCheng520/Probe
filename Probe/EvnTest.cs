28cefcda08e92d4b8872f2d5471d8c82658f31a3
JackCheng520 <302517623@qq.com>
2016-11-02 14:16:54 +0800

    探针系统

---
+++ Probe/EvnTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+// ================================
+//* 功能描述：EvnTest  
+//* 创 建 者：chenghaixiao
+//* 创建日期：2016/7/18 14:09:19
+// ================================
+namespace Assets.JackCheng.Probe
+{
+    public class EvnTest : MonoBehaviour
+    {
+        J_Echo probeSystem = new J_Echo();
+        string textFieldStr = "0";
+
+        void Start() {
+
+            probeSystem.Clear();
+            for (int i = 0; i < 100; i++)
+            {
+                probeSystem.Add(Setp, 0.1f);
+            }
+        }
+
+        void Update() 
+        {
+            probeSystem.Update();
+        }
+
+        void OnGUI()
+        {
+            //textFieldStr = GUI.TextField(new Rect(10, 10, 200, 30), textFieldStr);
+            //if (GUI.Button(new Rect(10, 50, 100, 60), "添加事件"))
+            //{
+            //    float t = float.Parse(textFieldStr);
+            //    probeSystem.Add(CallBack, t);
+            //}
+
+            //if (GUI.Button(new Rect(10, 50, 100, 60), "开始"))
+            //{
+            //    GoNext();
+            //}
+        }
+
+        void Setp() 
+        {
+            Debug.Log("Setp0 -- " + Time.realtimeSinceStartup);
+            GoNext();
+        }
+
+        void GoNext() {
+            //probeSystem.Go();
+        }
+    }
+}
---
+++ Probe/J_Echo.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// ================================
+//* 功能描述：J_Echo  
+//* 创 建 者：chenghaixiao
+//* 创建日期：2016/7/18 13:53:04
+// ================================
+namespace Assets.JackCheng.Probe
+{
+    public class J_Echo
+    {
+        public delegate void Msg();
+        class CallBack 
+        {
+            public Msg call;
+
+            public float delay;
+        }
+
+        private bool beLock = false;
+
+        List<CallBack> listMessage = new List<CallBack>();
+
+        public void Clear() {
+            listMessage.Clear();
+        }
+
+        public void Add(Msg _call) 
+        {
+            CallBack cb = new CallBack();
+            cb.call = _call;
+            cb.delay = 0;
+            listMessage.Add(cb);
+        }
+
+        public void Add(Msg _call, float _delay) 
+        {
+            CallBack cb = new CallBack();
+            cb.call = _call;
+            cb.delay = _delay;
+            listMessage.Add(cb);
+        }
+
+
+        public void Lock() {
+            beLock = true;
+        }
+        public void UnLock() {
+            beLock = false;
+        }
+
+        public void Update() {
+            if (!beLock) 
+            {
+                SearchEvn();
+            }
+        }
+        private Msg curMsg;
+        private void SearchEvn() 
+        {
+            if (listMessage.Count > 0) {
+
+                if (listMessage[0] != null) {
+                    listMessage[0].delay -= UnityEngine.Time.fixedDeltaTime;
+                    if (listMessage[0].delay <= 0) 
+                    {
+                        curMsg = listMessage[0].call;
+                        listMessage.RemoveAt(0);
+
+                        curMsg();
+                        curMsg = null;
+                    }
+                }
+            }
+        }
+
+
+    }
+}
